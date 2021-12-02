f9f0ff62ab56b90bf47f2a937e29c8e6e474faaa
jaimealess <jaimealessandro@mesoamericana.edu.gt>
2021-12-02 11:07:22 -0600

    se creo la tabla

--- alumno/admin.py
+++ alumno/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Alumno, AlumnoAdmin, Curso, CursoAdmin
 
 # Register your models here.
+
+admin.site.register(Alumno,AlumnoAdmin)
+admin.site.register(Curso, CursoAdmin)
